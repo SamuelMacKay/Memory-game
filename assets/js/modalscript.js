e18e43feb0af3d9159954e78a3177f2e1e74d838
Samuel MacKay <samuel.john.mackay@gmail.com>
2024-04-15 08:07:57 +0000

    added semi-colons at the lines missing them

--- assets/js/modalscript.js
+++ assets/js/modalscript.js
@@ -10,7 +10,7 @@ let gameOverSpan = document.getElementsByClassName("close")[1];
 // When the user clicks the button, open the modal 
 gameOverSpan.onclick = function() {
   showGameOverModal.style.display = "none";
-}
+};
 /* ------------------------------------------------------------------------Info Modal----*/
 
 // Get the modal
@@ -25,17 +25,17 @@ let span = document.getElementsByClassName("close")[0];
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
-}
+};
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
 
